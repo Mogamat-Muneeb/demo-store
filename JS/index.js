591429d17b0a628774e7a24051d985f7f59c13c7
isaacszubaidah <isaacszubaidah17@gmail.com>
2023-12-04 22:10:09 +0200

    wip

--- JS/index.js
+++ JS/index.js
@@ -7,10 +7,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Check if the loading overlay has been shown before
   if (!sessionStorage.getItem("loadingOverlayShown")) {
-    // If not shown before, display the loading overlay
-    document.getElementById("loadingOverlay").style.display = "block";
-    document.getElementById("pageContent").style.display = "none";
-
     // Set a flag in sessionStorage to indicate that the loading overlay has been shown
     sessionStorage.setItem("loadingOverlayShown", true);
 
@@ -18,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function () {
     setTimeout(function () {
       document.getElementById("loadingOverlay").style.display = "none";
       document.getElementById("pageContent").style.display = "block";
-    }, 1000);
+    }, 3000);
   } else {
     // If the loading overlay has been shown before, just display the page content
     document.getElementById("loadingOverlay").style.display = "none";
