049ae32672d7fbcaf228cd445a0b4fe1531589f0
Mogamat-Muneeb <davimuneeb785@gmail.com>
2023-12-06 13:04:21 +0200

    style single product

--- HTML/single_product_page.js
+++ HTML/single_product_page.js
@@ -1,27 +1,28 @@
+let purchased = JSON.parse(localStorage.getItem("purchased")) || [];
+const purchaseCount = document.getElementById("purchaseCount");
+purchaseCount.textContent = `${purchased.length}`;
 document.addEventListener("DOMContentLoaded", function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productId = parseInt(urlParams.get("id"));
-  
-    // Retrieve products from localStorage
-    const storedItems = JSON.parse(localStorage.getItem("items"));
-  
-    if (storedItems) {
-      // Find the product with the matching ID
-      const product = storedItems.find((product) => product.id === productId);
-  
-      if (product) {
-        // Update the HTML with the product details
-        document.getElementById("product-name").textContent = product.name;
-        document.getElementById("product-description").textContent = product.description;
-        document.getElementById("product-price").textContent = `Price: R${product.price}`;
-        // Update other elements with product details as needed
-      } else {
-        // If the product is not found, handle it (e.g., show an error message)
-        console.error("Product not found");
-      }
+  const urlParams = new URLSearchParams(window.location.search);
+  const productId = parseInt(urlParams.get("id"));
+
+  const storedItems = JSON.parse(localStorage.getItem("items"));
+
+  if (storedItems) {
+    const product = storedItems.find((product) => product.id === productId);
+
+    if (product) {
+      // Update the HTML with the product details
+      document.getElementById("product-name").textContent = product.name;
+      document.getElementById("product-description").textContent =
+        product.description;
+      document.getElementById(
+        "product-price"
+      ).textContent = `Price: R${product.price}`;
+      document.getElementById("product-image").src = product.url;
     } else {
-      // Handle the case where there are no products in localStorage
-      console.error("No products found in localStorage");
+      console.error("Product not found");
     }
-  });
-  
+  } else {
+    console.error("No products found in localStorage");
+  }
+});
